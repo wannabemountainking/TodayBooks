d6fbd5a3edc8866450bb28cef0683d3e738062eb
wannabemountainking <kdy0096@gmail.com>
2026-05-14 14:34:16 +0900

    Initial Commit

---
+++ TodayBooks/TodayBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayBooksApp.swift
+//  TodayBooks
+//
+//  Created by YoonieMac on 5/14/26.
+//
+
+import SwiftUI
+
+@main
+struct TodayBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
